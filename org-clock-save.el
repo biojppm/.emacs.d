d4b1094b1ec38dab2cb5f5272694170538540d6e
Joao Paulo Magalhaes <dev@jpmag.me>
2018-12-19 16:51:19 +0000

    org mode

---
+++ org-clock-save.el
@@ -0,0 +1,2 @@
+;; org-clock-save.el - dev1 at <2018-12-17 Mon 15:50>
+(setq org-clock-stored-history '(("d:/robodog/fdx.org" . 1832) ("d:/robodog/fdx.org" . 1956) ("d:/robodog/fdx.org" . 2085) ("d:/robodog/fdx.org" . 1140) ("d:/robodog/fdx.org" . 1329)))
