58eb82edb4c212ad91267ff232b49e0da85a4683
biojppm <dev@jpmag.me>
2019-06-10 23:50:08 +0200

    lsp: trying to defer loading

--- lisp/my-lsp-setup.el
+++ lisp/my-lsp-setup.el
@@ -7,22 +7,35 @@
   :init
   (message "lsp-mode INIT")
   (use-package lsp-ui
+    :defer t
     :commands lsp-ui-mode
     :init
+    (message "lsp-ui INIT")
     ;; https://github.com/emacs-lsp/lsp-ui/blob/master/lsp-ui-sideline.el
     (setq lsp-ui-sideline-delay 5.0)
     :bind
     ("C-c C-?" . lsp-ui-sideline-toggle-symbols-info)
+    (message "lsp-ui INIT")
     )
   (message "lsp-mode 1")
   (use-package company-lsp
+    :defer t
     :commands company-lsp)
   (message "lsp-mode 2")
   (setq lsp-enable-on-type-formatting nil)
+  ;; fix M-? fail: https://github.com/emacs-lsp/lsp-java/issues/122
+  (setq xref-prompt-for-identifier
+        '(not xref-find-definitions
+              xref-find-definitions-other-window
+              xref-find-definitions-other-frame
+              xref-find-references
+              )
+        )
   (message "lsp-mode DONE")
   )
 
 (use-package ccls
+  :defer t
   :after projectile
   ;;:ensure-system-package ccls ;; https://github.com/jwiegley/use-package/issues/698
   :custom
