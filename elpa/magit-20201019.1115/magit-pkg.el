fdbb67f4f7f3b34ab81fe53cc8adf83071200c38
biojppm <dev@jpmag.me>
2020-10-20 11:07:54 +0200

    update magit

--- elpa/magit-20201019.1115/magit-pkg.el
+++ elpa/magit-20201019.1115/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20200820.227" "A Git porcelain inside Emacs."
+(define-package "magit" "20201019.1115" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20200113")
     (dash "20200524")
     (git-commit "20200516")
     (transient "20200601")
     (with-editor "20200522"))
-  :commit "1ce4d1c0781b1925912704f3385128433f46d07c" :keywords
+  :commit "64341b02d24144b519a3f06362f0a7e839ef7189" :keywords
   '("git" "tools" "vc"))
 ;; Local Variables:
 ;; no-byte-compile: t
