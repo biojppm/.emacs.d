c71b9591c4e66e2a53c44246f02cb1d54a4e4760
biojppm <dev@jpmag.me>
2021-11-19 12:03:53 +0100

    update some packages

---
+++ elpa/git-commit-20211010.1635/git-commit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "git-commit" "20211010.1635" "Edit Git commit messages."
+  '((emacs "25.1")
+    (dash "20210826")
+    (transient "20210920")
+    (with-editor "20211001"))
+  :commit "9413847c1a085899d8de6f8d978bd7265f65e5d8" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li")
+    ("Sebastian Wiesner" . "lunaryorn@gmail.com")
+    ("Florian Ragwitz" . "rafl@debian.org")
+    ("Marius Vollmer" . "marius.vollmer@gmail.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
