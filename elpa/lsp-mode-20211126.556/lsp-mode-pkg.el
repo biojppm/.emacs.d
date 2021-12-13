25b687f1fd6d25d05788c50a6e7956e16a5e0f9b
Joao Paulo Magalhaes <dev@jpmag.me>
2021-12-13 19:29:27 +0000

    lsp-mode

--- elpa/lsp-mode-20211126.556/lsp-mode-pkg.el
+++ elpa/lsp-mode-20211126.556/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20211118.24" "LSP mode"
+(define-package "lsp-mode" "20211126.556" "LSP mode"
   '((emacs "26.1")
     (dash "2.18.0")
     (f "0.20.0")
@@ -6,7 +6,7 @@
     (spinner "1.7.3")
     (markdown-mode "2.3")
     (lv "0"))
-  :commit "9865d315123bb0482de358e6905838ba1acb5b45" :authors
+  :commit "ddb776037fc073473e0da9d07e63f57242b212fb" :authors
   '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
   :maintainer
   '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
