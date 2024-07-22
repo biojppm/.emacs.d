1f037d1098add1efb4f14fae1efb87c5bcbdc6db
Joao Paulo Magalhaes <dev@jpmag.me>
2024-07-22 19:11:58 +0200

    markdown fonts wtf!

--- init.el
+++ init.el
@@ -4001,13 +4001,16 @@ mode.
      )
    )
  )
+
+;; fixing faces: https://emacs.stackexchange.com/questions/42318/why-are-code-blocks-and-code-literals-displaying-with-large-face-in-markdown
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flymake-error ((t (:background nil :foreground nil :inverse-video nil :underline nil :slant normal :weight normal))))
- '(highlight-indentation-face ((t (:background "gray24")))))
+ '(highlight-indentation-face ((t (:background "gray24"))))
+ '(markdown-code-face ((t nil))))
 
 
 (set-face-attribute 'ido-vertical-first-match-face nil :background "#777777" :foreground "orange")
