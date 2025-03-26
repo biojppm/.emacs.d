41884f704594bc8b3021008092df24e34bce47ac
Joao Paulo Magalhaes <dev@jpmag.me>
2025-03-26 18:41:18 +0000

    clojure mode

--- init.el
+++ init.el
@@ -3933,12 +3933,139 @@ mode.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("80ceeb45ccb797fe510980900eda334c777f05ee3181cb7e19cd6bb6fc7fda7c" "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "ba9be9caf9aa91eb34cf11ad9e8c61e54db68d2d474f99a52ba7e87097fa27f5" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "77bd459212c0176bdf63c1904c4ba20fce015f730f0343776a1a14432de80990" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))
+   '("80ceeb45ccb797fe510980900eda334c777f05ee3181cb7e19cd6bb6fc7fda7c"
+     "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65"
+     "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc"
+     "ba9be9caf9aa91eb34cf11ad9e8c61e54db68d2d474f99a52ba7e87097fa27f5"
+     "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4"
+     "77bd459212c0176bdf63c1904c4ba20fce015f730f0343776a1a14432de80990"
+     "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553"
+     "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
+     "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223"
+     default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(clojure-mode ag anzu arduino-mode auto-highlight-symbol babel babel-repl backward-forward bicycle bitbake ccls clang-format clean-aindent-mode cmake-font-lock cmake-mode company-c-headers company-ycmd counsel-etags counsel-projectile cquery cython-mode dap-mode diminish dirtree dockerfile-mode drag-stuff dtrt-indent dumb-jump elisp-slime-nav elpy ess fd-dired find-file-in-project find-file-in-repository flatbuffers-mode flx-ido flycheck flycheck-rust flycheck-ycmd flymake-yaml forge fzf git-timemachine glsl-mode google-this hemisu-theme highlight-symbol hungry-delete ido-completing-read+ ido-hacks ido-vertical-mode idomenu iedit ivy ivy-searcher levenshtein lsp-mode lsp-python-ms lsp-ui lua-mode magit man-commands markdown-mode markdown-toc mc-extras modern-cpp-font-lock monokai-theme multiple-cursors nhexl-mode open-in-msvs paradox pdb-mode pdf-tools persp-mode persp-projectile php-mode powershell protobuf-mode pylint pytest-pdb-break realgud realgud-ipdb rg ripgrep rust-mode slime smart-mode-line smart-shift smartparens smex solarized-theme sqlite3 string-inflection sudo-edit syntax-subword tango-plus-theme term-run toml-mode transient undo-tree use-package vlf volatile-highlights vscode-dark-plus-theme web-mode wgrep wgrep-ag which-key window-number ws-butler yaml-mode yasnippet-classic-snippets zenburn-theme)))
+   '(
+     ag
+     anzu
+     arduino-mode
+     auto-highlight-symbol
+     babel
+     babel-repl
+     backward-forward
+     bicycle
+     bitbake
+     ccls
+     clang-format
+     clean-aindent-mode
+     clojure-mode
+     cmake-font-lock
+     cmake-mode
+     company-c-headers
+     company-ycmd
+     counsel-etags
+     counsel-projectile
+     cquery
+     cython-mode
+     dap-mode
+     diminish
+     dirtree
+     dockerfile-mode
+     drag-stuff
+     dtrt-indent
+     dumb-jump
+     elisp-slime-nav
+     elpy
+     ess
+     fd-dired
+     find-file-in-project
+     find-file-in-repository
+     flatbuffers-mode
+     flx-ido
+     flycheck
+     flycheck-rust
+     flycheck-ycmd
+     flymake-yaml
+     forge
+     fzf
+     git-timemachine
+     glsl-mode
+     google-this
+     hemisu-theme
+     highlight-symbol
+     hungry-delete
+     ido-completing-read+
+     ido-hacks
+     ido-vertical-mode
+     idomenu
+     iedit
+     ivy
+     ivy-searcher
+     levenshtein
+     lsp-java
+     lsp-mode
+     lsp-python-ms
+     lsp-ui
+     lua-mode
+     magit
+     man-commands
+     markdown-mode
+     markdown-toc
+     mc-extras
+     modern-cpp-font-lock
+     monokai-theme
+     multiple-cursors
+     nhexl-mode
+     open-in-msvs
+     paradox
+     pdb-mode
+     pdf-tools
+     persp-mode
+     persp-projectile
+     php-mode
+     powershell
+     protobuf-mode
+     pylint
+     pytest-pdb-break
+     realgud
+     realgud-ipdb
+     rg
+     ripgrep
+     rust-mode
+     slime
+     smart-mode-line
+     smart-shift
+     smartparens
+     smex
+     solarized-theme
+     sqlite3
+     string-inflection
+     sudo-edit
+     syntax-subword
+     tango-plus-theme
+     term-run
+     toml-mode
+     transient
+     undo-tree
+     use-package
+     vlf
+     volatile-highlights
+     vscode-dark-plus-theme
+     web-mode
+     wgrep
+     wgrep-ag
+     which-key
+     window-number
+     ws-butler
+     yaml-mode
+     yasnippet-classic-snippets
+     zenburn-theme
+     )
+   )
+ )
 
-;; fixing faces: https://emacs.stackexchange.com/questions/42318/why-are-code-blocks-and-code-literals-displaying-with-large-face-in-markdown
+;;
+fixing faces: https://emacs.stackexchange.com/questions/42318/why-are-code-blocks-and-code-literals-displaying-with-large-face-in-markdown
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
