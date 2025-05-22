32680586734f2a1585b025efad1c8e1621655edd
Joao Paulo Magalhaes <dev@jpmag.me>
2025-05-22 15:33:25 +0200

    ztree

--- init.el
+++ init.el
@@ -3961,7 +3961,8 @@ mode.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("80ceeb45ccb797fe510980900eda334c777f05ee3181cb7e19cd6bb6fc7fda7c"
+   '(
+     "80ceeb45ccb797fe510980900eda334c777f05ee3181cb7e19cd6bb6fc7fda7c"
      "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65"
      "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc"
      "ba9be9caf9aa91eb34cf11ad9e8c61e54db68d2d474f99a52ba7e87097fa27f5"
@@ -3970,7 +3971,9 @@ mode.
      "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553"
      "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
      "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223"
-     default))
+     default
+     )
+   )
  '(ispell-dictionary nil)
  '(package-selected-packages
    '(
@@ -4090,6 +4093,7 @@ mode.
      yaml-mode
      yasnippet-classic-snippets
      zenburn-theme
+     ztree
      )
    )
  )
