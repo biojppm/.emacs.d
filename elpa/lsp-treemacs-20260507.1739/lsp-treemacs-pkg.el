23cb449f22b2d9317448dc15c46deaa4875e46ed
Joao Paulo Magalhaes <dev@jpmag.me>
2026-05-13 17:39:34 +0100

    magit, lsp, treemacs

---
+++ elpa/lsp-treemacs-20260507.1739/lsp-treemacs-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-treemacs" "20260507.1739"
+  "LSP treemacs."
+  '((emacs    "29.1")
+    (dash     "2.18.0")
+    (f        "0.20.0")
+    (ht       "2.0")
+    (treemacs "2.5")
+    (lsp-mode "6.0"))
+  :url "https://github.com/emacs-lsp/lsp-treemacs"
+  :commit "0104931991803fa4a6eba0cf038007d98b8219bc"
+  :revdesc "010493199180"
+  :keywords '("languages"))
