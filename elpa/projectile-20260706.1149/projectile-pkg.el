d857d5552f0472ce99298493cb9fe59f4b91d227
Joao Paulo Magalhaes <dev@jpmag.me>
2026-07-08 00:34:17 +0100

    projectile

---
+++ elpa/projectile-20260706.1149/projectile-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "projectile" "20260706.1149"
+  "Manage and navigate projects in Emacs easily."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/bbatsov/projectile"
+  :commit "9bea34ed277a8cc30b0757d50e0d197c0ed7c849"
+  :revdesc "9bea34ed277a"
+  :keywords '("project" "convenience")
+  :authors '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
+  :maintainers '(("Bozhidar Batsov" . "bozhidar@batsov.dev")))
